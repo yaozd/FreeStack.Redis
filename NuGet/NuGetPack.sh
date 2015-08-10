aacc99f82c0f1a6b26ea70f29e0f9220c88dc765
barambani <filippo.mariotti@gmail.com>
2015-08-11 00:17:39 +0100

    Completed the nuget spec and the scripts to gernerate the release nuget package

--- NuGet/NuGetPack.sh
+++ NuGet/NuGetPack.sh
@@ -1,1 +1,1 @@
-mono ../src/.nuget/NuGet.exe pack ServiceStack.Redis/servicestack.redis.nuspec -symbols
+mono ../src/.nuget/NuGet.exe pack ServiceStack.Redis/servicestack.redis.nuspec -symbols  -BasePath ../
--- NuGet/NuGetPush.sh
+++ NuGet/NuGetPush.sh
@@ -1,2 +1,3 @@
-mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.0.0.nupkg
-mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.0.0.symbols.nupkg
+mono ../src/.nuget/NuGet.exe setApiKey 4f706108-1b6d-4a35-ac52-3d333137b4bd
+mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.nupkg
+mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.symbols.nupkg
