76918960c3158a5ff2727270964d2372c024fccb
barambani <filippo.mariotti@gmail.com>
2015-08-15 23:06:26 +0100

    Removed local reference from ServiceStack.Text. Updated nuget spec

--- NuGet/NuGetPush.sh
+++ NuGet/NuGetPush.sh
@@ -1,2 +1,2 @@
-mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.nupkg
-mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.symbols.nupkg
+mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.2.0.nupkg
+mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.2.0.symbols.nupkg
