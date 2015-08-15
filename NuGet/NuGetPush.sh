3349a0ab6ebcbd3a666285e376c581dfbaec6eff
barambani <barambani@users.noreply.github.com>
2015-08-15 21:27:12 +0100

    Update NuGetPush.sh

--- NuGet/NuGetPush.sh
+++ NuGet/NuGetPush.sh
@@ -1,3 +1,2 @@
-mono ../src/.nuget/NuGet.exe setApiKey 4f706108-1b6d-4a35-ac52-3d333137b4bd
 mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.nupkg
 mono ../src/.nuget/NuGet.exe push FreeStack.Redis.4.1.1.symbols.nupkg
